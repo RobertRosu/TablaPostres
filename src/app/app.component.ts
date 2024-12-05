1ce610057c4e891c3d115bf82ddd47823316fc52
RobertRosu <i_rosu@fpzornotza.com>
2024-12-05 09:34:32 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import postresList from 'src/assets/json/postres.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>URL Amigable</th>
+          <th>Imagen</th>
+          <th>Fecha</th>
+        </tr>
+      </thead>
+      <tbody *ngFor="let p of postres;">
+        <tr>
+          <td>{{p.id}}</td>
+          <td>{{p.nombre}}</td>
+          <td>{{p.stock}}</td>
+          <td>{{p.precio}}€</td>
+          <td>{{p.url}}</td>
+          <td><img src="../assets/img/{{p.img}}" alt="{{p.nombre}}"></td>
+          <td>{{p.fecha}}</td>
+        </tr>
+      </tbody>
+    </table>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TablaPostres';
+export class AppComponent implements OnInit{
+  postres: any = postresList
+
+  ngOnInit(): void {
+    console.log(this.postres)
+  }
 }
